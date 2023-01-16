1b14f58510c18e854ffa34b10837228ad80d3f3f
mahmoudelkomy996 <mahmoudelkomy996@gmail.com>
2023-01-16 10:51:10 +0200

    start

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+  //mainSwiper
+  var mainSwiper = new Swiper(".mainSwiper", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    // effect: "fade",
+    speed: 500,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".mainSwiperPagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".mainSwiperNext",
+      prevEl: ".mainSwiperPrev",
+    },
+  });
+  // nav menu
+  $(".navBtn").click(function () {
+    $(this).toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  });
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+});
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+$(document).ready(function () {
+  //spinner
+  $(".spinner").fadeOut("slow");
+  // odometer
+  $(".odometer").appear(function (e) {
+    var odo = $(".odometer");
+    odo.each(function () {
+      var countNumber = $(this).attr("data-count");
+      $(this).html(countNumber);
+    });
+  });
+  // aos
+  AOS.init({
+    offset: 60,
+    delay: 50,
+    duration: 1000,
+    // easing: "linear",
+    once: true,
+  });
+  //aos Delay
+  function aosDelay() {
+    var class_ = "mainSection";
+    $("section").each(function (i) {
+      class_ = $(this).attr("class");
+      if (class_) {
+        $("." + class_ + " div[data-aos]").each(function (i) {
+          var d = 0;
+          d = i * 100;
+          $(this).attr("data-aos-delay", d);
+          d = 0;
+        });
+      }
+    });
+  }
+  aosDelay();
+  // tooltip
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
